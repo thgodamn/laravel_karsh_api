68da08e2c17d32817ad69a331f853a463b7ef06b
Magomed Saybulaev <thgodamn@gmail.com>
2022-06-06 10:14:13 +0300

    CarController

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+use App\Models\User;
+
+class CarController extends Controller
+{
+    //
+    public function getcar(Request $request) {
+        $car = Car::find($request['car_id']);
+        $user = $car->user;
+
+        if ($user['api_token'] == $request['api_token']) {
+            if (isset($car['in_used'])) {
+                if ($car['in_used'] == 0) {
+                    $car->update(['in_used'=>1]);
+
+                    $user->update(['car_id'=>$car['id']]);
+
+                    return "Пользователь id: {$user['id']}, name: {$user['name']} - успешно арендовал машину";
+                } else {
+                    return 'Машина сейчас занята';
+                }
+            } else {
+                return 'Данной машины нет в базе';
+            }
+        } else {
+            return 'Неверный токен';
+        }
+    }
+
+    public function leavecar(Request $request) {
+        $user = User::find($request['user_id']);
+        $car = $user->car;
+
+        if ($user['api_token'] == $request['api_token']) {
+            if ($car['in_used'] == 1) {
+                $car->update(['in_used' => 0]);
+                return "Пользователь id: {$user['id']}, name: {$user['name']} - успешно вышел из машины";
+            } else {
+                return 'Пользователь не арендовал ни одну из машин';
+            }
+        } else {
+            return 'Неверный токен';
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Use App\Models\User;
 Use App\Models\Car;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\CarController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,45 +29,5 @@ Route::get('car/{id}', function($id) {
     return Car::find($id);
 });
 
-Route::post('getcar/',function (Request $request) {
-
-    $car = Car::find($request['car_id']);
-    $user = $car->user;
-
-    if ($user['api_token'] == $request['api_token']) {
-        if (isset($car['in_used'])) {
-            if ($car['in_used'] == 0) {
-                $car->update(['in_used'=>1]);
-
-                $user->update(['car_id'=>$car['id']]);
-
-                return "Пользователь id: {$user['id']}, name: {$user['name']} - успешно арендовал машину";
-            } else {
-                return 'Машина сейчас занята';
-            }
-        } else {
-            return 'Данной машины нет в базе';
-        }
-    } else {
-        return 'Неверный токен';
-    }
-});
-
-Route::post('leavecar/',function (Request $request) {
-
-    $user = User::find($request['user_id']);
-    $car = $user->car;
-
-    if ($user['api_token'] == $request['api_token']) {
-        if ($car['in_used'] == 1) {
-            $car->update(['in_used' => 0]);
-            return "Пользователь id: {$user['id']}, name: {$user['name']} - успешно вышел из машины";
-        } else {
-            return 'Пользователь не арендовал ни одну из машин';
-        }
-    } else {
-        return 'Неверный токен';
-    }
-
-});
-
+Route::post('leavecar/', [CarController::class, 'leavecar']);
+Route::post('getcar/', [CarController::class, 'getcar']);
